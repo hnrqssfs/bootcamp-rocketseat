24bce3f5144e37f89cfde51e9d8ec03e442a8df9
hnrqss <hnrq.assuncao@gmail.com>
2019-10-06 20:17:06 -0300

    connect to database

---
+++ modulo02/gobarber/src/database/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+
+import config from '../config/database';
+
+// models
+import User from '../app/models/User';
+
+const models = [User];
+
+class Database {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.connection = new Sequelize(config);
+
+        models.map(model => model.init(this.connection));
+    }
+}
+
+export default new Database();
--- modulo02/gobarber/src/routes.js
+++ modulo02/gobarber/src/routes.js
@@ -1,7 +1,21 @@
-const { Router } = require('express');
+import { Router } from 'express';
+
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ ok: true }));
+routes.get('/', async (req, res) => {
+    try {
+        const user = await User.create({
+            name: 'Henrique',
+            email: 'hnrq.assuncao@gmail.com',
+            provider: false,
+            password_hash: '123',
+        });
+        return res.json(user);
+    } catch (error) {
+        return res.status(400).res.json(error);
+    }
+});
 
 export default routes;
