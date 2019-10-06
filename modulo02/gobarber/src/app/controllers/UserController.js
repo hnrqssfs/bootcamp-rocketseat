6b9a6d31d316e8f79ccf977d5255b42e45803441
hnrqss <hnrq.assuncao@gmail.com>
2019-10-06 20:48:59 -0300

    User controller

---
+++ modulo02/gobarber/src/app/controllers/UserController.js
@@ -0,0 +1,30 @@
+import User from '../models/User';
+
+class UserController {
+    async store(req, res) {
+        try {
+            const userExist = await User.findOne({
+                where: { email: req.body.email },
+            });
+
+            if (userExist) {
+                return res.status(400).json({
+                    error: 'User already exist',
+                });
+            }
+
+            const { id, name, email, provider } = await User.create(req.body);
+
+            return res.json({
+                id,
+                name,
+                email,
+                provider,
+            });
+        } catch (error) {
+            return res.status(400).json(error);
+        }
+    }
+}
+
+export default new UserController();
--- modulo02/gobarber/src/routes.js
+++ modulo02/gobarber/src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    try {
-        const user = await User.create({
-            name: 'Henrique',
-            email: 'hnrq.assuncao@gmail.com',
-            provider: false,
-            password_hash: '123',
-        });
-        return res.json(user);
-    } catch (error) {
-        return res.status(400).res.json(error);
-    }
-});
+routes.post('/users', UserController.store);
 
 export default routes;
