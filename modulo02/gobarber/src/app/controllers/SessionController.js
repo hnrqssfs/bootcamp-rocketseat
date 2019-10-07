c354c25346c1f659b413a7bfb6e8b36593ef6b4a
hnrqss <hnrq.assuncao@gmail.com>
2019-10-06 21:24:39 -0300

    session controller

---
+++ modulo02/gobarber/src/app/controllers/SessionController.js
@@ -0,0 +1,51 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+const { secret, expiresIn } = authConfig;
+
+class SessionController {
+    async store(req, res) {
+        try {
+            const { email, password } = req.body;
+
+            const user = await User.findOne({
+                where: {
+                    email,
+                },
+            });
+
+            if (!user) {
+                return res.status(401).json({
+                    error: 'User not found',
+                });
+            }
+
+            if (!(await user.checkPassword(password))) {
+                return res.status(401).json({
+                    error: 'Password does not match',
+                });
+            }
+
+            const { id, name } = user;
+
+            return res.json({
+                user: {
+                    id,
+                    name,
+                    email,
+                },
+                token: jwt.sign({ id }, secret, {
+                    expiresIn,
+                }),
+            });
+        } catch (error) {
+            return res.status(400).json({
+                error: error.message,
+            });
+        }
+    }
+}
+
+export default new SessionController();
---
+++ modulo02/gobarber/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '6bbf73b03d00980d207e1a5341935f83',
+    expiresIn: '7d',
+};
