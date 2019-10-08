07294f125c9366b9eeee2bb14e97c062a7afb980
hnrqss <hnrq.assuncao@gmail.com>
2019-10-07 22:55:18 -0300

    update user

--- modulo02/gobarber/src/app/controllers/UserController.js
+++ modulo02/gobarber/src/app/controllers/UserController.js
@@ -28,13 +28,59 @@ class UserController {
 
     async update(req, res) {
         try {
+            const { email, oldPassword, password } = req.body;
+
             if (req.userId !== parseInt(req.params.id_user, 10)) {
                 return res.status(401).json({
                     error: 'User does not update another user',
                 });
             }
 
-            return res.json({ ok: true });
+            const user = await User.findOne({
+                where: {
+                    id: req.userId,
+                },
+            });
+
+            if (!user) {
+                return res.status(404).json({
+                    error: 'User not found',
+                });
+            }
+
+            if (email !== user.email) {
+                const userExist = await User.findOne({ where: { email } });
+
+                if (userExist) {
+                    return res.status(401).json({
+                        error: 'User already exists',
+                    });
+                }
+            }
+
+            if (oldPassword && !(await user.checkPassword(oldPassword))) {
+                return res.status(401).json({
+                    error: 'User passowrd does not match',
+                });
+            }
+
+            if (
+                password !== undefined &&
+                (oldPassword === undefined || oldPassword === null)
+            ) {
+                return res.status(401).json({
+                    error: 'User oldpassword does not match',
+                });
+            }
+
+            const { id, name, provider } = await user.update(req.body);
+
+            return res.json({
+                id,
+                name,
+                email,
+                provider,
+            });
         } catch (error) {
             return res.status(400).json(error.message);
         }
